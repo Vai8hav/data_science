2f2bb5c2613f88f8214e1254a5b2af48fadc13af
Vai8hav <43782376+Vai8hav@users.noreply.github.com>
2020-11-29 12:06:21 +0530

    Update cache matrix.R

--- cache matrix.R
+++ cache matrix.R
@@ -1,3 +1,7 @@
+# Function that set the matrix and the inverse in an environment
+# X is an invertible matrix
+# Pass the result of a makeCacheMatrix call to cacheSolve 
+
 makeCacheMatrix <- function(x = matrix()) 
   {
   inv <- NULL
@@ -12,6 +16,11 @@ makeCacheMatrix <- function(x = matrix())
        setinverse = setinverse,
        getinverse = getinverse)
 }
+
+# Compute and cache the inverse of a matrix
+# x is the result of a previous makeCacheMatrix call
+# (...) is used as additional arguments to pass to solve function
+
 cacheSolve <- function(x, ...) 
   {
   inv <- x$getinverse()
